15ea947f11db9996255f12e13013c97f20aac871
Eduard-Cristian Bolos <ebolos@thoughtworks.com>
2017-11-18 23:30:26 +0100

    Add more definitions

--- include/Arduino.h
+++ include/Arduino.h
@@ -47,6 +47,7 @@ extern "C" {
 #define OUTPUT 0x01
 #define SPECIAL           0xF8 //defaults to the usable BUSes uart0rx/tx uart1tx and hspi
 #define FUNCTION_0        0x08
+#define FUNCTION_1        0x18
 
 #define PI 3.1415926535897932384626433832795
 #define HALF_PI 1.5707963267948966192313216916398
@@ -54,6 +55,9 @@ extern "C" {
 #define DEG_TO_RAD 0.017453292519943295769236907684886
 #define RAD_TO_DEG 57.295779513082320876798154814105
 #define EULER 2.718281828459045235360287471352
+
+#define LSBFIRST 0
+#define MSBFIRST 1
 
 // Interrupt Modes
 #define RISING 0x01
